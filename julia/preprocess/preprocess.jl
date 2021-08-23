518c0c1723e3cd656d611241a1bd753087f58fc6
Sasha Petrenko <sap625@umsystem.edu>
2021-08-23 13:42:46 -0500

    Implement params loading, shuffle structs

--- julia/preprocess/preprocess.jl
+++ julia/preprocess/preprocess.jl
@@ -11,6 +11,7 @@ Authors:
 
 using Statistics: mean
 using StatsBase
+using DelimitedFiles
 
 """
     window_average(data::Array, bins::Int)
@@ -106,104 +107,3 @@ function task_feature_preprocess(x::Array, params::TaskDetectorParameters)
 
     return x_norm
 end # task_feature_preprocess(x::Array)
-
-# Feature preprocessing configuration
-const MEAN_FILE = "../config/mean/1.csv"
-const SCALE_FILE = "../config/scale/1.csv"
-const WINDOWS = 1
-const FEATURE_DIM = 128
-const SIGMOID_SCALING = 3.0
-
-# Load the mean and scale local configs
-mean = vec(readdlm(joinpath(local_path, mean_file), ',', Float64, '\n'))
-scale = vec(readdlm(joinpath(local_path, scale_file), ',', Float64, '\n'))
-
-# Construct the transformer from the mean and scale parameters
-transformer = ZScoreTransform(windows*feat_dim, 2, mean, scale)
-
-"""
-    PreprocessParameters
-
-Struct containing the options for the taskdetector.
-
-# Fields
-- `mean::Array{Float64, 1}`: means of each feature, used by transformer.
-- `scale::Array{Float64, 1}`: scaling parameter of each feature, used by transformer.
-- `transformer::ZScoreTransform`: object for transforming the data during preprocessing.
-- `windows::Int`: number of cells in each dimension (i.e., windows=2 results in 4 panels).
-- `feat_dim::Int`: dimension of each individual feature, before windowing.
-"""
-mutable struct PreprocessParameters
-    # Affine transformation parameters
-    mean::Array{Float64, 1}
-    scale::Array{Float64, 1}
-    transformer::ZScoreTransform
-
-    # Number of windows
-    # NOTE: this is actually number of cells in each dimension
-    #   e.g. windows = 2 results in four panels in the 2D image
-    windows::Integer
-
-    # Dimension of an individual feature, before considering windowing
-    feat_dim::Integer
-
-    # Sigmoid scaling parameter
-    sigmoid_scaling::Float64
-
-    # ART module options
-    opts_ddvfa::opts_DDVFA
-
-end # PreprocessParameters
-
-
-"""
-    PreprocessParameters(config::ConfParse)
-
-Default intialization for the task detector parameters.
-
-# Arguments
-- `config::ConfParse`: global configuration object with fields for preprocessing.
-"""
-function PreprocessParameters(config::ConfParse)
-
-    # Get the means and scaling parameters
-    mean_file = retrieve(config, "params", "mean_file", MEAN_FILE)
-    scale_file = retrieve(config, "params", "scale_file", SCALE_FILE)
-    # Get the number of windows
-    windows = parse(Int64, retrieve(config, "params", "windows", string(WINDOWS)))
-    # Get the feature dimension
-    feat_dim = parse(Int64, retrieve(config, "params", "feat_dim", string(FEATURE_DIM)))
-    sigmoid_scaling = parse(Float64, retrieve(config, "params", "sigmoid_scaling", string(SIGMOID_SCALING)))
-
-
-    # Load the mean and scale local configs
-    local_path = @__DIR__
-    mean = vec(readdlm(joinpath(local_path, mean_file), ',', Float64, '\n'))
-    scale = vec(readdlm(joinpath(local_path, scale_file), ',', Float64, '\n'))
-
-    # Construct the transformer from the mean and scale parameters
-    transformer = ZScoreTransform(windows*feat_dim, 2, mean, scale)
-
-    # Construct the DDVFA options from the taskdet_config.ini with defaults
-    opts_ddvfa = opts_DDVFA(
-        rho_lb    = parse(Float64,retrieve(config, "ddvfa", "rho_lb", string(DDVFA_RHO_LB))),
-        rho_ub    = parse(Float64,retrieve(config, "ddvfa", "rho_ub", string(DDVFA_RHO_UB))),
-        alpha     = parse(Float64,retrieve(config, "ddvfa", "alpha", string(DDVFA_ALPHA))),
-        beta      = parse(Float64,retrieve(config, "ddvfa", "beta", string(DDVFA_BETA))),
-        gamma     = parse(Float64,retrieve(config, "ddvfa", "gamma", string(DDVFA_GAMMA))),
-        gamma_ref = parse(Float64,retrieve(config, "ddvfa", "gamma_ref", string(DDVFA_GAMMA_REF))),
-        method    = retrieve(config, "ddvfa", "method", DDVFA_METHOD),
-        display   = parse(Bool, retrieve(config, "ddvfa", "display", string(DDVFA_DISPLAY)))
-    )
-
-    # Construct and return the parameters
-    PreprocessParameters(
-        mean,
-        scale,
-        transformer,
-        windows,
-        feat_dim,
-        sigmoid_scaling,
-        opts_ddvfa,
-    )
-end # PreprocessParameters(config::ConfParse)
