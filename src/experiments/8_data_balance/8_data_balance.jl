75dfa5c133a3aa24549b1469470590c858f9610b
Sasha Petrenko <sap625@umsystem.edu>
2022-08-05 09:55:58 -0500

    Dev data balance

---
+++ src/experiments/8_data_balance/8_data_balance.jl
@@ -0,0 +1,83 @@
+"""
+    8_balance.jl
+
+Description:
+    This script produces the Shannon entropy of the dataset to quantify the
+dataset balance/imbalance.
+
+Authors:
+- Sasha Petrenko <sap625@mst.edu>
+
+Timeline:
+- 7/29/2022: Created and documented.
+"""
+
+# -----------------------------------------------------------------------------
+# FILE SETUP
+# -----------------------------------------------------------------------------
+
+using Revise
+using DrWatson
+
+# Experiment save directory name
+experiment_top = "8_data_balance"
+
+# Run the common setup methods (data paths, etc.)
+include(projectdir("src", "setup.jl"))
+
+# -----------------------------------------------------------------------------
+# SCRIPT
+# -----------------------------------------------------------------------------
+
+# Manually entered sizes of the data
+data_sizes = Dict(
+    "DOTD" => Dict(
+        "train" => 337,
+        "test" => 348,
+    ),
+    "DOTM" => Dict(
+        "train" => 349,
+        "test" => 321,
+    ),
+    "EMAD" => Dict(
+        "train" => 296,
+        "test" => 279,
+    ),
+    "EMAM" => Dict(
+        "train" => 270,
+        "test" => 276,
+    ),
+    "PRD" => Dict(
+        "train" => 280,
+        "test" => 309,
+    ),
+    "PRM" => Dict(
+        "train" => 281,
+        "test" => 283,
+        # "test" => 2,
+    ),
+    # "" => Dict(
+    #     "train" =>,
+    #     "test" =>,
+    # ),
+)
+
+# Get the data vectors and other parameters
+train_vec = [data_sizes[key]["train"] for (key, _) in data_sizes]
+test_vec = [data_sizes[key]["test"] for (key, _) in data_sizes]
+n_classes = length(train_vec)
+n_train = sum(train_vec)
+n_test = sum(test_vec)
+# H = -sum()
+
+# OLD
+
+H_train = -sum(train_vec./n_train .* log.(train_vec/n_train))
+H_test = -sum(test_vec./n_test .* log.(test_vec/n_test))
+
+B_train = H_train / log(n_classes)
+B_test = H_test / log(n_classes)
+
+@info "Balances:" B_train B_test
+
+H =
--- src/setup.jl
+++ src/setup.jl
@@ -41,7 +41,7 @@ include(projectdir("src", "lib_c3.jl"))
 
 # Results directories (local and paper)
 results_dir(args...) = projectdir("work", "results", experiment_top, args...)
-paper_results_dir(args...) = joinpath("C:\\", "Users", "Sasha", "Dropbox", "Apps", "Overleaf", "Paper-MST-TDY-C3", "images", "results", experiment_top, args...)
+paper_results_dir(args...) = joinpath("C:\\", "Users", "Sasha", "Dropbox", "Apps", "Overleaf", "Paper-MST-TDY-C3-V3-Resubmission", "images", "results", experiment_top, args...)
 
 # Make the results directories if they do not exist
 mkpath(results_dir())
@@ -57,7 +57,9 @@ const DPI = 350
 const COLORSCHEME = :okabe_ito
 
 # Heatmap color gradient
-const GRADIENTSCHEME = pubu_9[5:end]
+if !isdefined(Main, :GRADIENTSCHEME)
+    const GRADIENTSCHEME = pubu_9[5:end]
+end
 # GRADIENTSCHEME = :thermal
 # GRADIENTSCHEME = ylgn_9
 # cgrad([:orange, :blue], [0.1, 0.3, 0.8])
@@ -79,6 +81,8 @@ const N_EB = 8
 const CONDENSED_LINEWIDTH = 2.5
 
 # colorbar_formatter
-const percentage_formatter = j -> @sprintf("%0.0f%%", 100*j)
+if !isdefined(Main, :percentage_formatter)
+    const percentage_formatter = j -> @sprintf("%0.0f%%", 100*j)
+end
 
 const PERCENTAGES_BOUNDS = (0.45, 1)
