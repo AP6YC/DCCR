c4f29b902be78270c8b3166a7ac4a7314d0358d1
Sasha Petrenko <sap625@umsystem.edu>
2022-09-28 17:36:15 -0500

    Dev l2metrics and logger

--- src/experiments/9_l2metrics/julia_l2metrics_dev.jl
+++ src/experiments/9_l2metrics/julia_l2metrics_dev.jl
@@ -25,14 +25,27 @@ using Logging
 # ENV["PYTHON"] = raw"C:\Users\Sasha\Anaconda3\envs\dccr\python.exe"
 # Pkg.build("PyCall")
 using PyCall
-
 @info PyCall.pyversion
 
-l2logger = pyimport("l2logger")
+# Import L2M Python libraries
+l2logger = pyimport("l2logger.l2logger")
 l2metrics = pyimport("l2metrics")
 
-@info l2logger
-@info l2metrics
+# Create the data logger
+data_logger = l2logger.DataLogger(
+    "work/results/9_l2metrics/logs",
+    "simple",
+    Dict(
+        "metrics_columns" => ["reward"],
+        "log_format_version" => "1.0"
+    ),
+    Dict(
+        "author" => "Sasha Petrenko",
+        "complexity" => "1-low",
+        "difficulty" => "2-medium",
+        "scenario_type" => "custom",
+    )
+)
 
 # Experiment save directory name
 # experiment_top = "9_condensed"
