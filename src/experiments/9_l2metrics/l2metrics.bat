154ad67c8f73338540323e535ebbcb5b342b5175
Sasha Petrenko <sap625@umsystem.edu>
2023-01-09 14:53:44 -0600

    Add metrics batch script

---
+++ src/experiments/9_l2metrics/l2metrics.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set dest_name=%1
+set src_log=%1
+
+set metrics=(performance)
+
+set out_dir=work\results\9_l2metrics\l2metrics\%dest_name%
+set src_dir=work\results\9_l2metrics\logs\%src_log%
+
+for %%m in %metrics% do (
+    python -m l2metrics -p %%m -o %%m -O %out_dir%/%%m -l %src_dir%
+)
