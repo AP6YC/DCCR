efc482b7c064ead85738ae151e207d6652e5ad3a
Sasha Petrenko <sap625@umsystem.edu>
2023-02-01 20:37:53 -0600

    Make l2m_to_table save correctly

--- src/experiments/9_l2metrics/5_l2m_to_table.jl
+++ src/experiments/9_l2metrics/5_l2m_to_table.jl
@@ -1,5 +1,6 @@
 using
     DrWatson,
+    DataFrames
     Revise,
     JSON,
     Latexify
@@ -15,17 +16,44 @@ desired_metrics = [
     "forward_transfer_ratio",
     "backward_transfer_ratio",
 ]
+pretty_rows = [
+    "Performance",
+    "Activation",
+    "Match",
+]
+
 # Point to the directory of the most recent metrics
 top_dir = readdir(results_dir("l2metrics"), join=true)[end]
 # Create a new destination dictionary for the metrics of interest
 new_metric_dict = Dict{String, Dict}()
 # Iterate over every metric in the most recent metrics dir
-for dir in readdir(top_dir)
+dirs = readdir(top_dir)
+# for dir in readdir(top_dir)
+new_metric_array = zeros(3, length(desired_metrics))
+for ix in eachindex(dirs)
+    dir = dirs[ix]
     # Parse the metric JSON file and add the desired entry to the new dict
     metric_file = joinpath(top_dir, dir, dir * "_metrics.json")
     metric_dict = JSON.parsefile(metric_file)
     new_metric_dict[dir] = Dict{String, Any}(
         des_met => metric_dict[des_met] for des_met in desired_metrics
     )
+    for jx in eachindex(desired_metrics)
+        new_metric_array[ix, jx] = metric_dict[desired_metrics[jx]]
+    end
 end
-println(new_metric_dict)
+
+# Make a dataframe with the array entries
+new_df = DataFrame(
+    Metric = pretty_rows,
+    PMM = new_metric_array[:, 1],
+    FTR = new_metric_array[:, 2],
+    BTR = new_metric_array[:, 3],
+)
+
+# Make a latex version of the dataframe and save
+new_df_tex = latexify(new_df, env=:table, fmt="%.3f")
+table_file = paper_results_dir("condensed.tex")
+open(table_file, "w") do f
+    write(f, new_df_tex)
+end
