5268fa59ce22af25aeb83140786a77f29510a8fc
Sasha Petrenko <sap625@mst.edu>
2023-02-07 09:55:52 -0600

    Add old tar, add cluster python env

--- src/set_pyenv.jl
+++ src/set_pyenv.jl
@@ -8,7 +8,9 @@ Sets the Python environment variable in an idempotent manner.
 # Point to the correct Python environment
 if !haskey(ENV, "PYTHON")
     # PYTHON_ENV = raw"C:\Users\Sasha\Anaconda3\envs\l2mmetrics\python.exe"
-    PYTHON_ENV = raw"C:\Users\sap62\Anaconda3\envs\l2m\python.exe"
+    # PYTHON_ENV = raw"C:\Users\sap62\Anaconda3\envs\l2m\python.exe"
+    # PYTHON_ENV = raw"C:\Users\sap62\Anaconda3\envs\l2m\python.exe"
+    PYTHON_ENV = raw"/home/sap625/miniconda3/envs/l2m/bin/python"
     if isfile(PYTHON_ENV)
         ENV["PYTHON"] = PYTHON_ENV
     else
