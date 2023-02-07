87c830b76e83e8a9f8aabf119d675637c9268d20
Sasha Petrenko <sap625@mst.edu>
2023-02-07 10:44:16 -0600

    New env

--- src/set_pyenv.jl
+++ src/set_pyenv.jl
@@ -10,7 +10,8 @@ if !haskey(ENV, "PYTHON")
     # PYTHON_ENV = raw"C:\Users\Sasha\Anaconda3\envs\l2mmetrics\python.exe"
     # PYTHON_ENV = raw"C:\Users\sap62\Anaconda3\envs\l2m\python.exe"
     # PYTHON_ENV = raw"C:\Users\sap62\Anaconda3\envs\l2m\python.exe"
-    PYTHON_ENV = raw"/home/sap625/miniconda3/envs/l2m/bin/python"
+    # PYTHON_ENV = raw"/home/sap625/miniconda3/envs/l2m/bin/python"
+    PYTHON_ENV = raw"/home/sap625/envs/l2m/bin/python"
     if isfile(PYTHON_ENV)
         ENV["PYTHON"] = PYTHON_ENV
     else
