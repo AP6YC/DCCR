adaa63556853afec0e073e2045390510c050017a
Sasha Petrenko <sap625@mst.edu>
2023-03-04 20:26:37 -0600

    Fix tiny imagenet usage

--- src/experiments/11_comparison/inits/tiny_imagenet.py
+++ src/experiments/11_comparison/inits/tiny_imagenet.py
@@ -1,12 +1,13 @@
 # Include the above folder
 import include_src
 
+from avalanche.benchmarks import SplitTinyImageNet
 from src.utils import scratchdir
 
 # Get the scratch directory
 dataset_root = scratchdir("tiny_imagenet")
 
-benchmark = avl.benchmarks.SplitTinyImageNet(
+benchmark = SplitTinyImageNet(
     n_experiences=10,
     return_task_id=True,
     dataset_root=dataset_root,
