3a6d0824889ca461720bef0ba7ad7971530a364f
Documenter.jl <documenter@juliadocs.github.io>
2023-06-05 13:00:58 +0000

    build based on b4b6719

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DCCR.jl","page":"DCCR.jl","title":"DCCR.jl","text":"","category":"section"},{"location":"","page":"DCCR.jl","title":"DCCR.jl","text":"Documentation for DCCR.jl","category":"page"}]
+}
