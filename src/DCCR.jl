e6da03e6da02d721ff18a4fe9fa4561a66cc65a6
Sasha Petrenko <sap625@umsystem.edu>
2023-06-05 10:19:37 -0500

    Add PrecompileSignatures

--- src/DCCR.jl
+++ src/DCCR.jl
@@ -1,7 +1,31 @@
 module DCCR
 
-include("version.jl")
+# -----------------------------------------------------------------------------
+# DEPENDENCIES
+# -----------------------------------------------------------------------------
 
+# Precompile concrete type methods
+using PrecompileSignatures: @precompile_signatures
+
+# -----------------------------------------------------------------------------
+# INCLUDES
+# -----------------------------------------------------------------------------
+
+# Include all files
+include("version.jl")   # Exported constant for the version of the project
+
+# -----------------------------------------------------------------------------
+# EXPORTS
+# -----------------------------------------------------------------------------
+
+# Export all public names
 export DCCR_VERSION
+
+# -----------------------------------------------------------------------------
+# PRECOMPILE
+# -----------------------------------------------------------------------------
+
+# Precompile any concrete-type function signatures
+@precompile_signatures(DCCR)
 
 end
--- src/deparallelize.jl
+++ src/deparallelize.jl
@@ -10,5 +10,6 @@ Timeline:
 - 1/15/2022: Created.
 - 2/17/2022: Documented.
 """
+
 # Close the workers after simulation
 rmprocs(workers())
