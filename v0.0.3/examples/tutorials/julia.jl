a5002a8f0b0795cc86a3aabfa9d9b9c7c4aeb39b
Documenter.jl <documenter@juliadocs.github.io>
2023-07-05 14:06:18 +0000

    build based on 78b31ca

---
+++ v0.0.3/examples/tutorials/julia.jl
@@ -0,0 +1,8 @@
+# `using` brings all of the names that are exported by the package into this context
+using Dates
+
+# `import` simply brings the name itself without exports, so to use sub components
+import Downloads
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
